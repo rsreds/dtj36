c24b8814613460450f24d6d29896c244216aeed7
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-05-25 20:26:41 +0200

    Fixed the color for real this time

--- scripts/score_popup.gd
+++ scripts/score_popup.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var text: String = "+1"
 @export var text_color: Color = Color.WHITE
 signal done
@@ -7,8 +8,8 @@ signal done
 func _ready() -> void:
 	$Label.text = text
 	$Label.add_theme_color_override("font_color", text_color)
-	$AnimationPlayer.play("pop_up")
-	await $AnimationPlayer.animation_finished
+	animation_player.play("pop_up")
+	await animation_player.animation_finished
 	done.emit()
 
 func play_sound()->void:
