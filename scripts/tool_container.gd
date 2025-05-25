c28e1cd0ba423810b7396165fb00c8fa539008af
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-05-25 16:23:15 +0200

    Add transition to tool menu

---
+++ scripts/tool_container.gd
@@ -0,0 +1,25 @@
+extends HBoxContainer
+
+var shown_position:Vector2 = Vector2(0,548)
+var hidden_position:Vector2 = Vector2(0,648)
+
+func anim_show() -> void:
+	var tween:Tween = create_tween()
+	tween.tween_property(self,
+	"position",
+	shown_position,
+	0.3).set_ease(Tween.EASE_IN_OUT)
+
+func anim_hide() -> void:
+	var tween:Tween = create_tween()
+	tween.tween_property(self,
+	"position",
+	hidden_position,
+	0.3).set_ease(Tween.EASE_IN_OUT)
+
+
+func _process(delta: float) -> void:
+	if GameManager.is_dragging and GameManager.object_being_dragged is PlanetNode:
+		anim_show()
+	if not GameManager.is_dragging:
+		anim_hide()
